97b30a27dc08f845ecbb0cd9df3b07b871a9a0e2
Justin Vrooman <vroomanj@veemon.local>
2015-02-21 22:19:49 -0500

    Add CSS

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219012051) do
+ActiveRecord::Schema.define(version: 20150222023745) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
